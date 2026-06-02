d20979867c43944ebf545723adab2a7625c63595
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2026-06-02 15:18:03 +0500

    WIP update notebook

--- estate/models/real_estate.py
+++ estate/models/real_estate.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class RealEstateProperty(models.Model):
@@ -10,7 +10,10 @@ class RealEstateProperty(models.Model):
     price = fields.Float(string='Price')
     bedrooms = fields.Integer(string='Bedrooms')
     bathrooms = fields.Integer(string='Bathrooms')
-    area = fields.Float(string='Area')
+    living_area = fields.Float(string='Living Area')
+    garden_area = fields.Float(string='Garden Area')
+    total_area = fields.Float(string='Total Area', compute='_compute_total_area')
+    best_price = fields.Float(string='Best Offer', compute='_compute_best_price')
     buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
     user_id = fields.Many2one(
         'res.users',
@@ -20,3 +23,13 @@ class RealEstateProperty(models.Model):
     property_type_id = fields.Many2one(comodel_name='estate.property.type', string='Property Type')
     offer_ids = fields.One2many(comodel_name='estate.property.offer', inverse_name='property_id', string='Offers')
     tag_ids = fields.Many2many(comodel_name='estate.property.tag', string='Tags')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'), default=0.0)
