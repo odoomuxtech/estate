3dabedcf65a8210a86b66d34943badb1ed958af9
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2026-06-02 15:35:20 +0500

    WIP validity date

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,6 +1,7 @@
+from datetime import date, timedelta
 from typing import Any, cast
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 _OFFER_STATUS_SELECTION = [
     ('accepted', 'Accepted'),
@@ -20,3 +21,30 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('real.estate', string='Property', required=True)
+    property_type_id = fields.Many2one(
+        related='property_id.property_type_id',
+        store=True,
+    )
+    create_date = fields.Datetime(string='Created on', readonly=True)
+    validity = fields.Integer(string='Validity (days)', default=7)
+    date_deadline = fields.Date(
+        string='Deadline',
+        compute='_compute_date_deadline',
+        inverse='_inverse_date_deadline',
+    )
+
+    def _get_offer_base_date(self) -> date:
+        self.ensure_one()
+        if self.create_date:
+            return self.create_date.date()
+        return fields.Date.today()
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for offer in self:
+            offer.date_deadline = offer._get_offer_base_date() + timedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            if offer.date_deadline:
+                offer.validity = (offer.date_deadline - offer._get_offer_base_date()).days
